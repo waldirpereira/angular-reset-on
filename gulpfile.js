e3885d7bd716f2a9919a12690f62c6105d5a22f0
Waldir J. Pereira Junior <waldirpereira@gmail.com>
2016-11-18 10:43:33 -0200

    modify gulpfile.js to not use ES6

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,22 @@
 'use strict';
 
 // Include gulp
-let gulp = require('gulp');
+var gulp = require('gulp');
 
 // Include Plugins
-let clean = require('gulp-clean');
-let jshint = require('gulp-jshint');
-let uglify = require('gulp-uglify');
-let rename = require('gulp-rename');
-let sourcemaps = require('gulp-sourcemaps');
-let filter = require('gulp-filter');
-let merge = require('merge-stream');
-let jasmine = require('gulp-jasmine');
-let karmaServer = require('karma').Server;
+var clean = require('gulp-clean');
+var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
+var filter = require('gulp-filter');
+var merge = require('merge-stream');
+var jasmine = require('gulp-jasmine');
+var karmaServer = require('karma').Server;
 
-let pathToKarmaConf = __dirname + '/test/conf/';
+var pathToKarmaConf = __dirname + '/test/conf/';
 
-let config = {
+var config = {
   src: './src/',
   dest: './dist/'
 };
@@ -56,8 +56,8 @@ gulp.task('clean-tests-lib', function () {
 
 // Copy test lib
 gulp.task('copy-tests-lib', ['clean-tests-lib'], function() {
-	let componentsPath = "bower_components/";
-	let testsLibPath = "test/lib/";
+	var componentsPath = "bower_components/";
+	var testsLibPath = "test/lib/";
 	return merge(
 		gulp.src(componentsPath + 'jasmine/lib/jasmine-core/*')
 			.pipe(gulp.dest(testsLibPath + 'jasmine')),
@@ -72,31 +72,13 @@ gulp.task('copy-tests-lib', ['clean-tests-lib'], function() {
 	);
 });
 
+// Test task (run Karma)
 gulp.task('test', function (done) {
   return new karmaServer({
     configFile: pathToKarmaConf + 'karma.conf.js',
     singleRun: true
   }, done).start();
 });
-
-/*
-gulp.task('test', function() {
-  // Be sure to return the stream
-  // NOTE: Using the fake './foobar' so as to run the files
-  // listed in karma.conf.js INSTEAD of what was passed to
-  // gulp.src !
-  return gulp.src('./foobar')
-    .pipe(karma({
-      configFile: pathToKarmaConf + 'karma.conf.js',
-      action: 'run'
-    }))
-    .on('error', function(err) {
-      // Make sure failed tests cause gulp to exit non-zero
-      console.log(err);
-      this.emit('end'); //instead of erroring the stream, end it
-    });
-});
-*/
 
 // Default Task
 gulp.task('default', ['scripts', 'copy-tests-lib']);
